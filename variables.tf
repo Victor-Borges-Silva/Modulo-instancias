f089b335fa6cf2f84d9a30f4600c56a5d2d1b7e4
Victor-Borges-Silva <victorborges975@gmail.com>
2024-06-29 20:02:57 -0300

    Primeiro commit

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "nome" {
+  type        = string
+  description = "Nome do recurso"
+}
+
+variable "ambiente" {
+  type        = string
+  default     = "Dev"
+  description = "Nome do recurso"
+}
